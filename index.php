d3a172c65d6b41adcb05ab9d4a9bb90db48de609
Аюпов Аяз <ayaz.ayupov@gmail.com>
2018-07-30 16:43:15 +0300

    autoloader

--- index.php
+++ index.php
@@ -7,6 +7,26 @@ use GraphQL\Type\Schema;
 
 try {
 
+    spl_autoload_register(
+        function($className)
+        {
+            $className = str_replace("_", "\\", $className);
+            $className = ltrim($className, '\\');
+            $fileName = '';
+            $namespace = '';
+            if ($lastNsPos = strripos($className, '\\'))
+            {
+                $namespace = substr($className, 0, $lastNsPos);
+                $className = substr($className, $lastNsPos + 1);
+                $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+            }
+            $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+            require $fileName;
+        }
+    );
+
+
     $rawInput = file_get_contents('php://input');
     $input = json_decode($rawInput, true);
     $query = $input['query'];
