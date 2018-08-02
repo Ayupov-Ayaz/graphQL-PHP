d47cb8e987eb4548b117c349fb2b4a979e19c71d
Аюпов Аяз <ayaz.ayupov@gmail.com>
2018-08-02 16:22:48 +0300

    using model User in MutationType

--- src/App/Models/User.php
+++ src/App/Models/User.php
@@ -1,6 +1,5 @@
 <?php
 namespace App\Models;
-use App\Types;
 use App\DB;
 class User
 {
@@ -22,5 +21,48 @@ class User
     public function getfriends(int $id) {
         return DB::select("SELECT u.* FROM friendships f JOIN users u ON u.id = f.friend_id 
                                  WHERE f.user_id = ".$id);
+    }
+
+    /**
+     *  Добавление пользователя
+     * @param array $user
+     * @return mixed
+     */
+    public function addUser(array $userParams) {
+        $userParams = $this->saveUserParams($userParams);
+        return DB::insert('users', $userParams);
+    }
+
+    /**
+     * Обновление  пользователя
+     * @param array $user
+     * @return mixed
+     */
+    public function updateUser(array $userParams) {
+        $userParams = $this->saveUserParams($userParams);
+        DB::update('users', $userParams, "id=" . $userParams['id']);
+        return DB::getElementById('users', $userParams['id']);
+    }
+
+    /**
+     *  Обработка переменных пользователя
+     * @param $params
+     * @return array
+     */
+    private function saveUserParams($params) {
+        // числовые типы
+        $intParams = array('id');
+        // строковые типы
+        $strParams = array('email', 'name');
+
+        $newParams = array();
+        foreach ($params as $key => $val) {
+            if(in_array($key, $intParams)) {
+                $newParams[$key] = (int) $val;
+            } else if(in_array($key, $strParams)) {
+                $newParams[$key] = "'".trim(htmlentities($val))."'" ;
+            }
+        }
+        return $newParams;
     }
 }
--- src/App/Type/MutationType.php
+++ src/App/Type/MutationType.php
@@ -3,13 +3,19 @@ namespace App\Type;
 
 
 use App\DB;
+use App\Models\User;
 use App\Types;
 use Exception;
 use GraphQL\Type\Definition\ObjectType;
 
 class MutationType extends ObjectType
 {
+    private static $userModel = null;
+
     public function __construct() {
+       if(is_null(self::$userModel)) {
+           self::$userModel = new User();
+       }
         $config = [
             'fields' => function() {
                 return [
@@ -21,10 +27,7 @@ class MutationType extends ObjectType
                             'name' => Types::nonNull(Types::string())
                         ],
                         'resolve' => function ($root, $args) {
-                            $id = (int)$args['id'];
-                            $name = htmlspecialchars(trim($args['name']));
-                            DB::update('users', ['name' => $name] , "id = {$id}");
-                            return DB::selectOne("SELECT * FROM users WHERE id = {$id}");
+                           return self::$userModel->updateUser($args);
                         }
                     ],
                     'changeUserEmail' => [
@@ -35,10 +38,7 @@ class MutationType extends ObjectType
                             'email' => Types::nonNull(Types::email())
                         ],
                         'resolve' => function($root, $args) {
-                            $id = (int)$args['id'];
-                            $email = trim(htmlentities($args['email']));
-                            DB::update('users', ['email' => $email], "id={$id}");
-                            return DB::selectOne("SELECT * FROM users WHERE id = {$id}");
+                            return self::$userModel->updateUser($args);
                         }
                     ],
                     'addUser' => [
@@ -48,10 +48,7 @@ class MutationType extends ObjectType
                             'user' => Types::inputUser()
                         ],
                         'resolve' => function($root, $args) {
-                            $userName = trim(htmlspecialchars($args['user']['name']));
-                            $userEmail = trim(htmlspecialchars($args['user']['email']));
-                            $userId = DB::insert("INSERT INTO users (name, email) VALUES ('$userName', '$userEmail')");
-                            return DB::selectOne("SELECT * FROM users WHERE id = $userId");
+                            return self::$userModel->addUser($args['user']);
                         }
                     ]
                 ];
