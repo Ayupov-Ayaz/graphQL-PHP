19e89b3a44deeba8e7145842ed88fe204f386de8
Аюпов Аяз <ayaz.ayupov@gmail.com>
2018-08-03 16:17:46 +0300

    changed model User

--- src/App/Models/User.php
+++ src/App/Models/User.php
@@ -1,16 +1,32 @@
 <?php
 namespace App\Models;
 use App\DB;
-class User
+use App\Services\Buffer;
+use App\Services\BufferedType;
+use GraphQL\Deferred;
+
+class User implements BufferedType
 {
     /**
+     * Ключи для определения функционала для использования буфера
+     * @var array
+     */
+    private $keys = [
+        'getCountFriends' => 'ufc'
+    ];
+    /**
      * Получить количество друзей пользователя
-     * @param int $id
+     * @param array $args
      * @return mixed
      */
-    public function getCountFriends($id) {
-        return DB::affectingStatement("SELECT u.* FROM friendships f  JOIN users u ON u.id = f.friend_id 
-                                             WHERE f.user_id = ".$id);
+    public function getCountFriends($args) {
+        Buffer::addId($this->keys['getCountFriends'], $args->id);
+
+        return new Deferred(function() use ($args){
+            $this->loadData($this->keys['getCountFriends']);
+            $result = Buffer::getResult($this->keys['getCountFriends']);
+            return $result[$args->id];
+        });
     }
 
     /**
@@ -64,5 +80,31 @@ class User
             }
         }
         return $newParams;
+    }
+
+    /**
+     * Загрузка данных в буфер
+     * @param $key
+     */
+    public function loadData($key) {
+        $ids = Buffer::getIds($key);
+
+        $countFriends = function  ($key, $ids){
+            $rows = DB::select("SELECT u.id, COUNT(f.friend_id) AS count 
+                                     FROM users u LEFT JOIN friendships f ON f.user_id = u.id 
+                                     WHERE u.id IN (" . implode(',', $ids) . ") GROUP BY u.id");
+            foreach($rows as $row) {
+                $param = [
+                    $row->id => (int)$row->count
+                ];
+                Buffer::addResult($key, $param);
+            }
+        };
+
+        switch ($key) {
+            case $this->keys['getCountFriends']:
+                $countFriends($key, $ids); break;
+            // другие функции использующие буфер
+        }
     }
 }
